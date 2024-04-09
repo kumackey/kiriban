20afad25e4f0d58e9ebb88b7ea77b9359fa977a4
kumackey <kumak1t09e0@gmail.com>
2024-04-09 22:45:49 +0900

    コメントを出すところも自作

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/google/go-github/github"
 	"github.com/kumackey/kiriban/kiriban"
+	"golang.org/x/oauth2"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -19,5 +23,42 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	fmt.Print(c.IsKiriban(prNumber))
+	if !c.IsKiriban(999) { // test
+		fmt.Printf("#%d is not kiriban.\n", prNumber)
+		os.Exit(0)
+	}
+
+	fmt.Printf("#%d is kiriban!\n", prNumber)
+
+	ctx := context.Background()
+
+	token, ok := os.LookupEnv("GITHUB_TOKEN")
+	if !ok {
+		log.Fatalln("GITHUB_TOKEN is not set.")
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	repository, ok := os.LookupEnv("GITHUB_REPOSITORY")
+	if !ok {
+		log.Fatalln("GITHUB_REPOSITORY is not set.")
+	}
+	parts := strings.Split(repository, "/")
+	owner := parts[0]
+	repo := parts[1]
+
+	comment := &github.IssueComment{Body: github.String(
+		fmt.Sprintf("Congratulations! #%d is kiriban! 🎉", prNumber),
+	)}
+
+	ic, _, err := client.Issues.CreateComment(ctx, owner, repo, prNumber, comment)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Commented: %s\n", *ic.HTMLURL)
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,18 @@ module github.com/kumackey/kiriban
 
 go 1.22.2
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/stretchr/testify v1.8.4
+	golang.org/x/oauth2 v0.19.0
+)
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
