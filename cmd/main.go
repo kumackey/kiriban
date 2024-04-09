983529eaa1d8937589f842d6d6ff7837ede20023
kumackey <kumak1t09e0@gmail.com>
2024-04-09 15:55:53 +0900

    コメントする

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/kumackey/kiriban"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	args := os.Args[1:]
-	for _, arg := range args {
-		fmt.Println(arg)
+	prNumber, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	prNumber = 999
+
+	c, err := kiriban.NewChecker()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("::set-output name=is_kiriban::%t\n", c.IsKiriban(prNumber))
 }
